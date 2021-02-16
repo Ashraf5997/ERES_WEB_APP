7b3305850f8787cbc31e98ec9758588354a0c475
Ashraf_Jamal <ashrafjamal5997@gmail.com>
2021-02-16 16:57:50 +0530

    done

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -238,7 +238,7 @@ return(
        <div className="main_div_addE">
          <div className="center_div_addE">
            <p className="p1" style={{backgroundColor:'rgb(11, 5, 102)'}}>
-                      <BsFillPersonPlusFill /> Add Employee Data [GITHUB]
+                      <BsFillPersonPlusFill /> Add Employee Data [s]
            </p> <hr />
            <Form onSubmit = { handleSubmit }>
                <Row>
